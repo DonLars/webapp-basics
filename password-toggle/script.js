ec5c2ddc9b808b6be6505881733f0b7131d0d891
Lars Wagner <lars@tubies.de>
2023-11-20 19:03:39 +0100

    feat: add show/hide function with setAttribute

--- password-toggle/script.js
+++ password-toggle/script.js
@@ -1,1 +1,22 @@
 "use strict";
+
+const input = document.querySelector("input");
+const btn = document.querySelector("button");
+let showHidePassword = false;
+
+btn.addEventListener("click", function () {
+  showHidePassword = !showHidePassword;
+
+  const type = input.getAttribute("type");
+  if (showHidePassword) {
+    input.setAttribute("type", "text");
+    btn.innerText = "Show Password";
+  } else {
+    input.setAttribute("type", "password");
+    btn.innerText = "Hide Password";
+  }
+});
+
+function preventFormSubmission() {
+  return false;
+}
