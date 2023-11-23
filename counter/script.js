7eef7f3039e9e14146eea593b64517ff8e914d96
Lars Wagner <lars@tubies.de>
2023-11-23 11:28:25 +0100

    fix: changes

---
+++ counter/script.js
@@ -0,0 +1,26 @@
+"use strict";
+
+// Add a larger counter number
+
+let counter = 0;
+let divider = 100;
+
+let counterBar = document.querySelector(".counter");
+
+counterBar.innerHTML = "xxx";
+
+for(let i = 0; i ) {
+    
+}
+/*
+ The number is increased every time you
+    - click the main area of the website
+    - press the enter or space key
+ */
+
+// Add a reset button that will reset the number to 0
+
+/*Change the color of the main area behind your counter step by step everytime the counter 
+ is increased 100 counting steps will turn the background fully colorful*/
+
+// After every 100 counting steps reset the background (see video)
--- fast-cheap-good/script.js
+++ fast-cheap-good/script.js
@@ -1,1 +1,54 @@
 "use strict";
+
+// Selektiere die Checkboxen mit den entsprechenden IDs
+const fastCheckbox = document.querySelector("#fast");
+const cheapCheckbox = document.querySelector("#cheap");
+const goodCheckbox = document.querySelector("#good");
+
+// Variable, um das zuletzt ausgewählte Kontrollkästchen zu verfolgen
+let lastCheckedCheckbox = null;
+
+// Funktion zum Umschalten des ausgewählten Kontrollkästchens
+function toggle(clickedCheckbox) {
+  // Deklariere Variablen für die beiden anderen Checkboxen in der Gruppe
+  let firstOtherCheckbox, secondOtherCheckbox;
+
+  // Überprüfe, welches Kontrollkästchen angeklickt wurde, um die anderen zu bestimmen
+  if (fastCheckbox !== clickedCheckbox) {
+    firstOtherCheckbox = fastCheckbox;
+    if (cheapCheckbox !== clickedCheckbox) {
+      secondOtherCheckbox = cheapCheckbox;
+    } else {
+      secondOtherCheckbox = goodCheckbox;
+    }
+  } else {
+    firstOtherCheckbox = cheapCheckbox;
+    secondOtherCheckbox = goodCheckbox;
+  }
+
+  // Überprüfe, ob alle drei Checkboxen ausgewählt sind
+  if (
+    clickedCheckbox.checked &&
+    firstOtherCheckbox.checked &&
+    secondOtherCheckbox.checked
+  ) {
+    // Deaktiviere das zuletzt ausgewählte Kontrollkästchen
+    lastCheckedCheckbox.checked = false;
+  }
+
+  // Aktualisiere die Variable für das zuletzt ausgewählte Kontrollkästchen
+  lastCheckedCheckbox = clickedCheckbox;
+}
+
+// Füge Event-Listener für Klick-Ereignisse zu den Checkboxen hinzu
+fastCheckbox.addEventListener("click", function () {
+  toggle(fastCheckbox);
+});
+
+cheapCheckbox.addEventListener("click", function () {
+  toggle(cheapCheckbox);
+});
+
+goodCheckbox.addEventListener("click", function () {
+  toggle(goodCheckbox);
+});
