545b0c7eda6132666176e17367debfc4afc3a674
Lars Wagner <lars@tubies.de>
2023-12-05 20:14:37 +0100

    fix: add first task, counter, claim

--- firsttodo/script.js
+++ firsttodo/script.js
@@ -1,57 +1,67 @@
 "use strict";
 
-/* TODO: Demo Todo, Counter, css,  */
-
-/*    SETTING UP REFERENCES
+/*    STAGING & GET ELEMENTS
 ========================================================================== */
-const taskForm = document.getElementById("task-form");
-const input = document.getElementById("input");
-const taskList = document.getElementById("task-list");
-const clearButton = document.getElementById("clear-btn");
+const taskForm = document.getElementById("task-form"); // reference to form
+const input = document.getElementById("input"); // reference to inputfield
+const taskList = document.getElementById("task-list"); // reference to task list
+const clearButton = document.getElementById("clear-btn"); // reference to clear button
+const countTask = document.querySelector("count-task"); // reference to counter
+const claim = document.querySelector(".claim"); // reference to claim
 let currentFilter = "all"; // "all", "done", "open"
-
 let tasks = JSON.parse(localStorage.getItem("tasks")) || []; // load tasks from local storage OR set a new array
+
+// Add a default "testTodo" if there are no tasks in local storage
+if (tasks.length === 0) {
+  const testTodo = {
+    id: new Date().getTime(), // Get time for create an id
+    description: "Start your first task 😜",
+    isDone: false,
+  };
+  tasks.push(testTodo); // push into tasks
+  localStorage.setItem("tasks", JSON.stringify(tasks)); // save to localStorage
+}
 
 /*    EVENT LISTENER - SUBMIT BUTTON
 ========================================================================== */
 taskForm.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  // Check for duplicates
+  // Check for duplicates, if there are some tasks which
   const isDuplicate = tasks.some(
     (task) =>
-      task.description.toLowerCase() === input.value.trim().toLowerCase() // check if inputValue is in tasks
+      task.description.toLowerCase() === input.value.trim().toLowerCase() // check if task in array === inputValue
   );
 
   if (isDuplicate) {
     alert("Sorry, you can't add a duplicate task!");
   } else {
     if (input.value.trim() !== "") {
-      // 1. State updaten
+      // if field is NOT clear update State
       tasks.push({
-        id: new Date().getTime(),
-        description: input.value.trim(),
+        id: new Date().getTime(), // Get time for create an id
+        description: input.value.trim(), // Get input field, trim whitespaces before and after
         isDone: false,
       });
 
-      // 2. Redisplay triggern
+      // Render state
       onStateChange();
 
-      input.value = "";
-      input.focus(); // Focus on input field for the next task
+      input.value = ""; // Clear input field
+      input.focus(); // Focus input field for the next task
     } else {
-      return;
+      return; // do nothing
     }
   }
 });
 
 /*    EVENT LISTENER - FILTER
 ========================================================================== */
-const filterRadios = document.querySelectorAll('input[name="filter"]');
+const filterRadios = document.querySelectorAll('input[name="filter"]'); // select radio inputs
 filterRadios.forEach((radio) => {
   radio.addEventListener("change", function () {
-    currentFilter = this.value;
-    display();
+    currentFilter = radio.value; // set currentfilter to this value
+    onStateChange();
   });
 });
 
@@ -59,64 +69,56 @@ filterRadios.forEach((radio) => {
 ========================================================================== */
 clearButton.addEventListener("click", function () {
   if (confirm("Do you really want to delete the complete list?")) {
-    tasks = [];
+    tasks = []; // clear complete array after alert windows
   }
   onStateChange();
 });
 
-/*    FUNCTION - DISPLAY
+/*    FUNCTION - render
 ========================================================================== */
-function display() {
-  taskList.innerHTML = "";
+function render() {
+  taskList.innerHTML = ""; // Clear taskList
 
   // Filter
   const filteredTodos = tasks.filter((task) => {
     if (currentFilter == "done") {
-      return task.isDone;
+      return task.isDone; // set task to isDone
     }
     if (currentFilter == "open") {
-      return !task.isDone;
+      return !task.isDone; // set task to NOT isDone
     }
-    return true;
+    return true; // all
   });
 
   // Building list element with for each
   filteredTodos.forEach(function (task) {
-    const listItem = document.createElement("li");
-    listItem.setAttribute("id", task.id);
-    listItem.classList.add("task-item");
+    const listItem = document.createElement("li"); // create list element
+    listItem.setAttribute("id", task.id); // add id to list element
+    listItem.classList.add("task-item"); // add task-item class to list element
     listItem.innerHTML = `
     <div class="checkbox-wrapper">
-      <input type="checkbox" name="is-done" class="toggle-complete" id="${
+      <input type="checkbox" name="is-done" class="toggle-complete" id="checkbox-${
         task.id
       }" ${task.isDone ? "checked" : ""}>
-      <label for="${task.id}">${task.description}</label>
+      <label for="checkbox-${task.id}">${task.description}</label>
       </div>
       <button class="delete-task" id="${task.id}">X</button>
-    `;
-    taskList.append(listItem);
+    `; // add HTML template with the complete list item, checkbox, label, delete Button
+
+    taskList.append(listItem); // append to UL List
   });
 
   // EVENTLISTENER - CHECKBOXES
-  const checkboxes = document.querySelectorAll(".toggle-complete");
+  const checkboxes = document.querySelectorAll(".toggle-complete"); // get all checkboxes
   checkboxes.forEach((checkbox) => {
     checkbox.addEventListener("change", () => {
-      const taskId = checkbox.id;
-      const currentTask = tasks.find((task) => task.id === parseInt(taskId));
+      const taskId = parseInt(checkbox.id.replace("checkbox-", "")); // entfernen der checkbox ID
+      const currentTask = tasks.find((task) => task.id === taskId); // find in tasks the current Task by compare the IDs
 
       // Änderungen für Checkboxen
-      if (checkbox.checked) {
-        checkbox.setAttribute("checked", "");
-        checkbox.classList.add("check");
-      } else {
-        checkbox.removeAttribute("checked");
-      }
+      currentTask.isDone = checkbox.checked; // set current Task to the checked checkbox
 
-      // Change State
-      currentTask.isDone = checkbox.checked;
-
-      // Speichern im Local Storage und aktualisieren der Anzeige
-      onStateChange();
+      onStateChange(); // Save in localStorage and update the display
     });
   });
 
@@ -127,18 +129,33 @@ function display() {
     button.addEventListener("click", () => {
       const taskId = button.id;
       tasks = tasks.filter((removeTask) => removeTask.id !== parseInt(taskId));
-      localStorage.setItem("tasks", JSON.stringify(tasks)); // in storage speichern
-      document.getElementById(taskId).remove(); // gewählte ID aus dem DOM löschen
+      localStorage.setItem("tasks", JSON.stringify(tasks)); // Save to localStorage
+      document.getElementById(taskId).remove(); // delete choosen ID from DOM
     });
   });
 }
 
-/*    FUNCTION - ON STAGE (SAVE + DISPLAY WRAPPER)
+/*    FUNCTION - ON STAGE (SAVE + Render WRAPPER)
 ========================================================================== */
 function onStateChange() {
-  //    Save to localstorage
-  localStorage.setItem("tasks", JSON.stringify(tasks));
-  display();
+  localStorage.setItem("tasks", JSON.stringify(tasks)); // Save to localStorage
+  render();
+  currentTaskCount();
 }
 
-onStateChange();
+/*    COUNT OPEN TASK - FUNCTION
+========================================================================== */
+
+function currentTaskCount() {
+  const openTasks = tasks.filter((task) => !task.isDone); // Filter all tasks, if not isDone
+
+  if (openTasks.length > 0) {
+    claim.textContent = `Getting ${openTasks.length} things done…`;
+  } else {
+    claim.textContent = `Great, all tasks are completed!`;
+  }
+  countTask.textContent = openTasks.length; // Update the countTask element
+}
+
+onStateChange(); // Initial call to set up the initial state
+currentTaskCount(); // Initial call to set up the initial count
